1d9e53c95b8bcb12cac919b2b2d989b39abea27f
{pavan5116} <{pawankalyanvarikuppala2276@gmail.com}>
2026-02-12 10:36:30 +0530

    local to public

--- project/project/settings.py
+++ project/project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-m0hnhk2zzqab+y7m%rt3&aopr8sm8n&)zjvips9y6-slni&sz7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []   #['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
